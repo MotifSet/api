f7b1a998c7ab036a5a37e2953ea9c42ba952d664
Nick Erokhin <nickerokhin@gmail.com>
2018-10-06 13:29:21 -0700

    Implemented coincap api, updated package.json

--- src/services/coinCapService.ts
+++ src/services/coinCapService.ts
@@ -1,14 +1,43 @@
-import SetProtocol, { Address, SignedIssuanceOrder, TakerWalletOrder } from 'setprotocol.js';
-import * as _ from 'lodash';
+import * as request from 'superagent';
+import * as express from 'express';
+const baseUrl = "https://api.coinmarketcap.com/v2/";
 
-import { constants } from '../utils/constants';
-const { PUBLIC_ADDRESS, SET_KOVAN_ADDRESSES  } = constants;
+var coinTickerIdMap: {[symbol: string]: number} = {
+    "ICX": 2099,
+    "ZRX": 1896,
+    "ZIL": 2469,
+    "AE": 1700,
+    "BAT": 1697,
+    "REP": 1104,
+    "SNT": 1759,
+    "PPT": 1789,
+    "LOOM": 2588,
+    "VET": 3077,
+    "DAI": 2308
+};
 
 export class CoinCapService {
-    private _provider: Provider;
-    constructor(provider: Provider) {
-        this._provider = provider;
+
+    async getTokenInfo(req: express.Request, res: express.Response): Promise<void>{
+        var token = req.query.symbol;
+        var resp;
+        var tokenId: number = coinTickerIdMap[token];
+        request
+            .get(baseUrl + "ticker/" + coinTickerIdMap[token] + "/")
+            .set("Accept", "application/json")
+            .end(function(error, response){
+                if (error || !response.ok){
+                    res.status(404).send(JSON.stringify(res, null, 2));
+
+                } else{
+                    resp = response.body;
+                    var formattedResp = {
+                        "symbol": resp["Symbol"],
+                        "price": resp["quotes"]["price"]
+                    }
+                    res.status(200).send(JSON.stringify(formattedResp, null, 2))
+                }
+            });
+
     }
-    
-    // TODO
 }
