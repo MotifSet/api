125a38e76365a128173a14c6917e01105b76a851
Nick Erokhin <nickerokhin@gmail.com>
2018-10-07 02:28:00 -0700

    Updated sets

--- src/utils/sets.ts
+++ src/utils/sets.ts
@@ -1,24 +1,108 @@
 export const SETS = [
     {
         name: 'S&P 500 Index',
-        symbol: 'SNP',
+        symbol: 'SPY',
         address: '0x2c58a14de96b522502857818e4dcc9b07a3993c4',
         naturalUnits: 10,
         price: Math.random() * 10 + 10,
         components: [
             {
-                name: 'Dai',
-                symbol: 'DAI',
+                name: 'S&P 500|Augur',
+                symbol: 'aSPY',
                 address: '0x1d82471142F0aeEEc9FC375fC975629056c26ceE',
                 units: 5,
             },
             {
-                name: 'TrueUSD',
-                symbol: 'TUSD',
+                name: 'Short Eth',
+                symbol: 'sEth',
                 address: '0xAdB015D61F4bEb2A712D237D9d4c5B75BAFEfd7B',
                 units: 5,
             },
         ],
     },
+    {
+        name: 'Gold',
+        symbol: 'GLD',
+        address: '0x2c58a14de96b522502857818e4dcc9b07a3993c4',
+        naturalUnits: 10,
+        price: Math.random() * 10 + 10,
+        components: [
+            {
+                name: 'Gold|Augur',
+                symbol: 'aGLD',
+                address: '0x1d82471142F0aeEEc9FC375fC975629056c26ceE',
+                units: 5,
+            },
+            {
+                name: 'Short Eth',
+                symbol: 'sETH',
+                address: '0xAdB015D61F4bEb2A712D237D9d4c5B75BAFEfd7B',
+                units: 5,
+            },
+        ],
+    },
+    {
+        name: 'FANG+ Index',
+        symbol: 'FANG',
+        address: '0x2c58a14de96b522502857818e4dcc9b07a3993c4',
+        naturalUnits: 10,
+        price: Math.random() * 10 + 10,
+        components: [
+            {
+                name: 'FANG+|Augur',
+                symbol: 'aFANG',
+                address: '0x1d82471142F0aeEEc9FC375fC975629056c26ceE',
+                units: 5,
+            },
+            {
+                name: 'Short Eth',
+                symbol: 'sETH',
+                address: '0xAdB015D61F4bEb2A712D237D9d4c5B75BAFEfd7B',
+                units: 5,
+            },
+        ],
+    },
+    {
+        name: 'Biotechnology',
+        symbol: 'IBB',
+        address: '0x2c58a14de96b522502857818e4dcc9b07a3993c4',
+        naturalUnits: 10,
+        price: Math.random() * 10 + 10,
+        components: [
+            {
+                name: 'Biotechnology|Augur',
+                symbol: 'aIBB',
+                address: '0x1d82471142F0aeEEc9FC375fC975629056c26ceE',
+                units: 5,
+            },
+            {
+                name: 'Short Eth',
+                symbol: 'sETH',
+                address: '0xAdB015D61F4bEb2A712D237D9d4c5B75BAFEfd7B',
+                units: 5,
+            },
+        ],
+    },
+    {
+        name: 'US Real Estate',
+        symbol: 'VNQ',
+        address: '0x2c58a14de96b522502857818e4dcc9b07a3993c4',
+        naturalUnits: 10,
+        price: Math.random() * 10 + 10,
+        components: [
+            {
+                name: 'US Real Estate|Augur',
+                symbol: 'aVNQ',
+                address: '0x1d82471142F0aeEEc9FC375fC975629056c26ceE',
+                units: 5,
+            },
+            {
+                name: 'Short Eth',
+                symbol: 'sETH',
+                address: '0xAdB015D61F4bEb2A712D237D9d4c5B75BAFEfd7B',
+                units: 5
+            },
+        ],
+    }
 
 ]
